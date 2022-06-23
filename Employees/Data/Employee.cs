17b78c984633ae821c47b638a77d16d4ca14f3f5
Trevor Brown <developer@thebrowncrew.us>
2022-06-23 13:12:09 -0400

    Employee data and operations separation
    Created data structures for employees and required access operations.
    Removed unnecessary template code and file.
    Added endpoints and functionality to satisfy requirements.

---
+++ Employees/Data/Employee.cs
@@ -0,0 +1,38 @@
+﻿namespace Employees;
+
+public record Employee
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Department { get; set; }
+    public DateTime HireDate { get; set; }
+}
+
+public class EmployeeData
+{
+    private static List<Employee> _employees = new()
+    {
+        new Employee { Id = 1, FirstName = "Alberta", LastName = "Jackson", Department = "Finance", HireDate = new DateTime(2007, 6, 5) },
+        new Employee { Id = 2, FirstName = "Alicia", LastName = "Bennett", Department = "Human Resources", HireDate = new DateTime(2001, 4, 15) },
+        new Employee { Id = 3, FirstName = "Donna", LastName = "Avent", Department = "Revenue", HireDate = new DateTime(2009, 4, 20) },
+        new Employee { Id = 4, FirstName = "Duane", LastName = "Holder", Department = "Human Services", HireDate = new DateTime(2020, 8, 15) }
+    };
+
+
+    public static List<Employee> GetEmployees()
+    {
+        return _employees;
+    }
+
+    public static Employee? GetEmployee(int id)
+    {
+        return _employees.SingleOrDefault(employee => employee.Id == id);
+    }
+
+    public static Employee CreateEmployee(Employee newEmployee)
+    {
+        _employees.Add(newEmployee);
+        return newEmployee;
+    }
+}
--- Employees/Program.cs
+++ Employees/Program.cs
@@ -1,51 +1,34 @@
-﻿namespace Employees;
+﻿using Employees;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
 {
-    public static void Main(string[] args)
+    options.SwaggerDoc("v1", new OpenApiInfo
     {
-        var builder = WebApplication.CreateBuilder(args);
+        Version = "v1",
+        Title = "Wake County Employee API",
+        Description = "ASP.NET 6.x Web API for Wake County employee management."
+    });
+});
 
-        // Add services to the container.
-        builder.Services.AddAuthorization();
+var app = builder.Build();
 
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-        {
-            var forecast =  Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = summaries[Random.Shared.Next(summaries.Length)]
-                })
-                .ToArray();
-            return forecast;
-        })
-        .WithName("GetWeatherForecast");
-
-        app.Run();
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee API V1");
+    });
 }
 
+// Add endpoints
+app.MapGet("/employees", () => EmployeeData.GetEmployees());
+app.MapGet("/employees/{id}", (int id) => EmployeeData.GetEmployee(id));
+app.MapPost("/employees", (Employee newEmployee) => EmployeeData.CreateEmployee(newEmployee));
+
+app.Run();
